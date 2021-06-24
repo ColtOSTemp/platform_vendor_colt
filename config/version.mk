662308b862872e694fa2b1feb01ec5465116d89a
Mrinal Ghosh <mg712702@gmail.com>
2021-06-24 22:35:12 +0530

    vendor: Build Updates package
     * Only for official devices
    
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- config/version.mk
+++ config/version.mk
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2021 The ColtOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # ColtOS Versioning System
 COLT_MAJOR_VERSION = 11.0
 COLT_RELEASE_VERSION = v7.0
@@ -14,6 +30,10 @@ ifeq ($(COLT_BUILD_TYPE), Official)
       COLT_BUILD_TYPE := Unofficial
       $(error Device is not official "$(COLT_BUILD)")
     endif
+
+PRODUCT_PACKAGES += \
+    Updates
+
 endif
 
 # System version
@@ -26,6 +46,7 @@ COLT_DATE_HOUR := $(shell date -u +%H)
 COLT_DATE_MINUTE := $(shell date -u +%M)
 
 COLT_BUILD_DATE := $(COLT_DATE_YEAR)$(COLT_DATE_MONTH)$(COLT_DATE_DAY)-$(COLT_DATE_HOUR)$(COLT_DATE_MINUTE)
+COLT_BUILD_DATE_UTC := $(shell date -d '$(COLT_DATE_YEAR)-$(COLT_DATE_MONTH)-$(COLT_DATE_DAY) $(COLT_DATE_HOUR):$(COLT_DATE_MINUTE) UTC' +%s)
 COLT_BUILD_VERSION := $(COLT_RELEASE_VERSION)
 COLT_BUILD_FINGERPRINT := ColtOS/$(COLT_MOD_VERSION)/$(TARGET_PRODUCT_SHORT)/$(COLT_BUILD_DATE)
 COLT_VERSION := ColtOS-$(COLT_MAJOR_VERSION)-$(COLT_BUILD_VERSION)-$(COLT_BUILD)-$(COLT_BUILD_TYPE)-$(COLT_BUILD_DATE)
@@ -36,6 +57,7 @@ PRODUCT_GENERIC_PROPERTIES += \
   ro.colt.build.version=$(COLT_BUILD_VERSION) \
   ro.colt.build.type=$(COLT_BUILD_TYPE) \
   ro.colt.build.date=$(COLT_BUILD_DATE) \
+  ro.colt.build_date_utc=$(COLT_BUILD_DATE_UTC) \
   ro.colt.build.fingerprint=$(COLT_BUILD_FINGERPRINT) \
   ro.colt.build.maintainer=$(COLT_BUILD_MAINTAINER) \
   ro.colt.main.version=$(COLT_MAJOR_VERSION)
